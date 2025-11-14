1b17f788e08d3167ee32ac2b770d447a549e9edb
Christian Bromann <git@bromann.dev>
2025-11-14 05:44:38 -0800

    fix(langchain): update JS docs on toolRetryMiddleware (#1414)
    Updates to `toolRetryMiddleware` in JS

--- pipeline/preprocessors/link_map.py
+++ pipeline/preprocessors/link_map.py
@@ -365,6 +365,7 @@ LINK_MAPS: list[LinkMap] = [
             "RunsClient.stream": "classes/_langchain_langgraph-sdk.client.RunsClient.html#stream",
             "ClearToolUsesEdit": "classes/langchain.index.ClearToolUsesEdit.html",
             "ContextEdit": "interfaces/langchain.index.ContextEdit.html",
+            "toolRetryMiddleware": "functions/langchain.index.toolRetryMiddleware.html",
         },
     },
 ]
